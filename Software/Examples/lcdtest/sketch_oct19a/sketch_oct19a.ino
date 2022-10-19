0d563f9761c1b3b9078202a04a207dd6640386cc
Leo Cosgrove <leoco341@gmail.com>
2022-10-19 12:49:26 -0400

    adding example lcd code

---
+++ Software/Examples/lcdtest/sketch_oct19a/sketch_oct19a.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C ldc(0x3F, 16, 2);
+// address could be 0x27 also
+
+void setup() {
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+
+  lcd.setCursor(0,0);
+  lcd.print("hello world");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
